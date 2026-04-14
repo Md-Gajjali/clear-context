54598ac6bf5ee25601c0a981cf3bb519bccf1e4e
Md-Gajjali <hellogajjali@gmail.com>
2026-04-14 23:59:41 +0600

    add  theme

---
+++ src/Context/ThemeContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react'
+export const ThemeProvider = createContext()
+const ThemeContext = ({children}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ThemeContext
