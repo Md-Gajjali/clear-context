28a442a47f972b2b12076b9475dde2debe5737b6
Md-Gajjali <hellogajjali@gmail.com>
2026-04-15 00:03:41 +0600

    update   theme

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,6 +1,22 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 export const ThemeProvider = createContext()
 const ThemeContext = ({children}) => {
+    const [theme,setTheme]=useState("light")
+
+    const toggle = () => {
+        setTheme(
+            theme === "light" ? "dark" : "light"
+        )
+
+        let html = document.querySelector("html")
+
+        if (theme === "light") {
+            html.classList.add("dark")
+        } else {
+            html.classList.remove("dark")
+            
+        }
+    }
   return (
     <div>
       
